1d19affae3f1b042a1f53741376687a6c820a811
Augusto Vinicius <37191927+AugustoViniciusCarvalho@users.noreply.github.com>
2020-04-15 16:28:21 -0300

    Add files via upload

--- js/artistasinfo.js
+++ js/artistasinfo.js
@@ -2,5 +2,7 @@ window.onload = console.log(localStorage.getItem("e"));
 
 let e = localStorage.getItem("e");
 
+let texto = document.getElementById("texto");
 let imagem = document.getElementById("imagem");
-imagem.src=`imagens/${e}hd.png`
+imagem.src=`imagens/${e}hd.png`;
+texto.src=`textos/${e}.txt`;
