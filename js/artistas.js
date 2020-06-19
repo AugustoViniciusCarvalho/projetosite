6b328070e768fc02146d1a2bcf4227f1a491c247
Augusto Vinicius <37191927+AugustoViniciusCarvalho@users.noreply.github.com>
2020-06-19 15:25:28 -0300

    Add files via upload

--- js/artistas.js
+++ js/artistas.js
@@ -1,50 +1,76 @@
-function info(e){
-    console.log(localStorage.setItem("e", e.target.id));
-    window.location.href="artistasinfo.html"
+function info(e) {
+  console.log(localStorage.setItem("e", e.target.id));
+  window.location.href = "artistasinfo.html"
 }
 
 window.onload = console.log(localStorage.getItem("t"));
 
 let t = localStorage.getItem("t");
 
-if (t == "todos"){
+if (t == "todos") {
   $("div.diretor").show();
   $("div.ator").show();
   $("div.dramaturgo").show();
-}else if (t == "diretor"){
-    $("div.diretor").show();
-    $("div.ator").hide();
-    $("div.dramaturgo").hide();
-}else if(t == "ator"){
-    $("div.diretor").hide();
-    $("div.ator").show();
-    $("div.dramaturgo").hide();
-}else if(t == "dramaturgo"){
+} else if (t == "diretor") {
+  $("div.diretor").show();
+  $("div.ator").hide();
+  $("div.dramaturgo").hide();
+
+  sumir("div.dramaturgo");
+  sumir("div.ator");
+  sumir("span.dramaturgo");
+  sumir("span.ator");
+
+} else if (t == "ator") {
+  $("div.diretor").hide();
+  $("div.ator").show();
+  $("div.dramaturgo").hide();
+
+  sumir("div.dramaturgo");
+  sumir("div.diretor");
+  sumir("span.dramaturgo");
+  sumir("span.diretor");
+
+} else if (t == "dramaturgo") {
   $("div.diretor").hide();
   $("div.ator").hide();
   $("div.dramaturgo").show();
 
+  sumir("div.diretor");
+  sumir("div.ator");
+  sumir("span.diretor");
+  sumir("span.ator");
+}
+
+function sumir(string){
+  for (let i = 0; i < $(string).length + 1; i++) {
+    for (let i = 0; i < $(string).length; i++) {
+      let id = $(string)[i].id;
+      let dramaturgo = document.getElementById(id);
+      dramaturgo.remove();
+    }
+  }
 }
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
 
-  function tipo(t){
-    console.log(localStorage.setItem("t", t.target.id));
-    window.location.href="artistas.html"
+function tipo(t) {
+  console.log(localStorage.setItem("t", t.target.id));
+  window.location.href = "artistas.html"
 }
--- js/dramaturgiainfo.js
+++ js/dramaturgiainfo.js
@@ -5,9 +5,59 @@ let e = localStorage.getItem("e");
 let texto = document.getElementById("texto");
 let imagem = document.getElementById("imagem");
 imagem.src = `Obras Imagens/${e}.png`;
-texto.src=`Obras textos/${e}.txt`;
+texto.src = `Obras textos/${e}.txt`;
 
-console.log(e);
+function lerCompleto(){
+  if (e == "aabelinhasonhadora"){
+    window.location.href = "https://drive.google.com/file/d/1iGlIZP4fEweX3DLlM9Rp7u8h0nHh4XXa/view?usp=sharing";
+  } else if (e == "aguerradosplanetas"){
+    window.location.href = "https://drive.google.com/file/d/1R081_ZQujUP6SNUWifsGnt85sepyNonS/view?usp=sharing";
+  } else if (e == "aloucadamontanha"){
+    window.location.href = "https://drive.google.com/file/d/1M-4kmZ-nKXEMRki_dFEdjXI2Q5icSa97/view?usp=sharing";
+  } else if (e == "amanhaquemsabe"){
+    window.location.href = "https://drive.google.com/file/d/18JVDRSydfUDfo_p15KtGVqTG4xfkcMOM/view?usp=sharing";
+  } else if (e == "aprovidencia"){
+    window.location.href = "https://drive.google.com/file/d/1BIHwmeDFKDHBXgFpEvl4WS56QVrsdIio/view?usp=sharing";
+  } else if (e == "astresdatas"){
+    window.location.href = "https://drive.google.com/file/d/1bgjm29MMP1ZzQC3dDe_YdqK54q8KTZ1J/view?usp=sharing";
+  } else if (e == "aloucadamontanha"){
+    window.location.href = "https://drive.google.com/file/d/1M-4kmZ-nKXEMRki_dFEdjXI2Q5icSa97/view?usp=sharing";
+  } else if (e == "avemaria"){
+    window.location.href = "https://drive.google.com/file/d/1716gOeZ-J2VwdHvr54XEgakh9gQTDR8F/view?usp=sharing";
+  } else if (e == "brasileiroseportugueses"){
+    window.location.href = "https://drive.google.com/file/d/1EaJuRIvA6Aljewxg2qtr5yfUq1MIkVnw/view?usp=sharing";
+  } else if (e == "cabecadomundo"){
+    window.location.href = "https://drive.google.com/file/d/1BKQDRYrBloyATmL8rN8uOcygC1ODnKMv/view?usp=sharing";
+  } else if (e == "caodefila"){
+    window.location.href = "https://drive.google.com/file/d/1wUSy9zFEXc4Xx7f5IUKVy83MAD3Lsnz2/view?usp=sharing";
+  } else if (e == "compraseummarido"){
+    window.location.href = "https://drive.google.com/file/d/1Ls57tIxR0ge7b_SGvQL-YslQPr-QFwXD/view?usp=sharing";
+  } else if (e == "dialogodasestrelas"){
+    window.location.href = "https://drive.google.com/file/d/13Tg_vzP-vvB1oMYomCKK2uDPhsTgzgVk/view?usp=sharing";
+  } else if (e == "formacaorural"){
+    window.location.href = "https://drive.google.com/file/d/1yuLmRpdvBmfFJHxFhpdQ4V9SEXKQIyka/view?usp=sharing";
+  } else if (e == "oanjodameianoite"){
+    window.location.href = "https://drive.google.com/file/d/1eg7ro-dqRO6o0EBLIHqYbZEPB1yf1jCk/view?usp=sharing";
+  } else if (e == "ocarrasco"){
+    window.location.href = "https://drive.google.com/file/d/1Oq_M4ZCuaRl-hRMT_vftbA57hpW-yBfL/view?usp=sharing";
+  } else if (e == "omenordosmeusirmaos"){
+    window.location.href = "https://drive.google.com/file/d/1RBgSt2l6yZCdXp8GFN9DEfCMQykgQuZp/view?usp=sharing";
+  } else if (e == "onatalnoespacosideral"){
+    window.location.href = "https://drive.google.com/file/d/1WoPzg1bM0N57R2SUhhTRa6fN3PKi5Pgw/view?usp=sharing";
+  } else if (e == "osanjosdoclaustro"){
+    window.location.href = "https://drive.google.com/file/d/1PLQb7_UpIUNIGZ1hNitIANb0563YbdoM/view?usp=sharing";
+  } else if (e == "pelasgrades"){
+    window.location.href = "https://drive.google.com/open?id=18d7KxjLRgAyBeYBuptDbGnuSLJpzsL4s";
+  } else if (e == "pertinhodoceu"){
+    window.location.href = "https://drive.google.com/open?id=1OadEmV7pT8VJrSZ2U16s6PC9EdTKrNnh";
+  } else if (e == "seusoldonalua"){
+    window.location.href = "https://drive.google.com/file/d/1kW584CwU0XJvmaJMGwMejk6PanPDjnVp/view?usp=sharing";
+  } else if (e == "umjardimchamadogetsemani"){
+    window.location.href = "https://drive.google.com/file/d/1q0eMAjkWWn3lB5pMWU8sfS33psEnPrzn/view?usp=sharing";
+  } else if (e == "umrestodehomem"){
+    window.location.href = "https://drive.google.com/file/d/1bZqj1FKfDmTMewMav1S9_3jG3y2r34gI/view?usp=sharing";
+  }
+}
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -30,8 +80,4 @@ function myFunction() {
   function tipo(t){
     console.log(localStorage.setItem("t", t.target.id));
     window.location.href="artistas.html"
-}
-
-function lerCompleto(){
-  window.location.href="https://drive.google.com/file/d/1q0eMAjkWWn3lB5pMWU8sfS33psEnPrzn/view"
 }
