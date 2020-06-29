7a1dc0ccc7a9c6d38f3a3aa15223831a2b614dc3
Augusto Vinicius <37191927+AugustoViniciusCarvalho@users.noreply.github.com>
2020-06-29 14:14:14 -0300

    Add files via upload

---
+++ galeriadoartista.js
@@ -0,0 +1,30 @@
+window.onload = console.log(localStorage.getItem("i"));
+let i = localStorage.getItem("i");
+
+    $("span").not(`.${i}`).hide();
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function tipo(t){
+    console.log(localStorage.setItem("t", t.target.id));
+    window.location.href="artistas.html";
+}
+
