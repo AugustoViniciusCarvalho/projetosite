fcf15c9065dc528ac5a05d76ce637b01436f86c3
Augusto Vinicius <37191927+AugustoViniciusCarvalho@users.noreply.github.com>
2020-07-29 10:54:03 -0300

    Add files via upload

--- js/teatros.js
+++ js/teatros.js
@@ -1,25 +1,7 @@
-window.onload = console.log(localStorage.getItem("e"));
-let e = localStorage.getItem("e");
-
-  $("li").not(`.${e}`).hide();
-  $(".apareca").show();
-
-
-function sumir(string){
-  for (let i = 0; i < $(string).length + 1; i++) {
-    for (let i = 0; i < $(string).length; i++) {
-      let id = $(string)[i].id;
-      let dramaturgo = document.getElementById(id);
-      dramaturgo.remove();
-    }
-  }
-}
-
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-  // Close the dropdown if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -35,7 +17,5 @@ function myFunction() {
 
   function tipo(t){
     console.log(localStorage.setItem("t", t.target.id));
-    window.location.href="artistas.html";
-  }
-
-  
+    window.location.href="artistas.html"
+}
